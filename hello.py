efba92b03bd5f47ebe2b2ad616e6f1dc241e29ac
kiranmint82 <kiranmint82@gmail.com>
2026-04-14 13:21:13 +0530

    hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+# hello.py
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print()
+print("Hello,", name + "!")
+print("You are", age, "years old.")
+print("Welcome to Python!")
